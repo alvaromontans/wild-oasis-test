f6c001cc23cbf1b1daba1c64cc94873828fbc196
alvaromon <alvaromifp@gmail.com>
2025-03-21 15:05:13 +0100

    Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import { analyzer } from 'vite-bundle-analyzer'
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // analyzer()
   ],
   build: {
     rollupOptions: {
