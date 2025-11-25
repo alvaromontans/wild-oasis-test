a8e9b3978f00423ed7df5689abd8619b051a1855
alvaromon <alvaromifp@gmail.com>
2025-11-25 09:52:23 +0100

    Configuracion supabase

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
-export const supabaseUrl = 'https://hboyocbxemswijajyjfl.supabase.co'
+export const supabaseUrl = 'https://vlonwvizvdymyfzrooow.supabase.co'
 const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
